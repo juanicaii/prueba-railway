e2b63bea616228f931fb71789b1aaef5b8667667
TamboriniAgustin <tamboriniagustin00@gmail.com>
2022-06-04 15:13:33 -0300

    Inicialización del proyecto

---
+++ src/main/java/com/tecnico/accenture/AccentureApplication.java
@@ -0,0 +1,16 @@
+package com.tecnico.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureApplication.class, args);
+		
+		System.out.println("Aplicación iniciada correctamente...");
+		
+	}
+
+}
